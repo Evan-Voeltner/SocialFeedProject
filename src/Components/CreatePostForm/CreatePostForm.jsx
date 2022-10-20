d7b4f2ad10f91768c565b6d32b985f6c19d60b7f
Evan Voeltner <evanvoeltner@gmail.com>
2022-10-20 10:07:17 -0500

    Index.js & app.jss are properly configured + createpostform.jsx is created

---
+++ src/Components/CreatePostForm/CreatePostForm.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const AddEntryForm = (props) => {
+  return (
+    <form>
+      <label>Name</label>
+      <input type="text"></input>
+      <label>post</label>
+      <input type="text"></input>
+    </form>
+  );
+};
+
+export default AddEntryForm;
