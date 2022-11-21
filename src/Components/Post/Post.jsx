ea095a7c15e2b13711fe0a8d6a0ccfb9a43c92d3
Evan Voeltner <evanvoeltner@gmail.com>
2022-11-20 19:16:10 -0600

    feat: Post component renders a Like and Dislike button that turns their respective colors when selected

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,10 +1,36 @@
 import React, { useState } from "react";
+import "./Post.css";
 
 const Post = (props) => {
+  const [likeButtonClass, setLikeButtonClass] = useState('inactive-like-button');
+  const [dislikeButtonClass, setDislikeButtonClass] = useState('inactive-dislike-button');
+
+  function handleLikeClick(){
+    if(likeButtonClass === 'inactive-like-button'){
+      setLikeButtonClass("active-like-button");
+    }
+    else{
+      setLikeButtonClass("inactive-like-button");
+    }
+  }
+
+  function handleDislikeClick(){
+    if(dislikeButtonClass === 'inactive-dislike-button'){
+      setDislikeButtonClass("active-dislike-button");
+    }
+    else{
+      setDislikeButtonClass("inactive-dislike-button");
+    }
+  }
+
   return (
     <div>
       <h1>{props.post.name}</h1>
       <p>{props.post.post}</p>
+      <span>
+        <button className={likeButtonClass} onClick={handleLikeClick}>Like</button>
+        <button className={dislikeButtonClass} onClick={handleDislikeClick}>Dislike</button>
+      </span>
     </div>
   );
 };
